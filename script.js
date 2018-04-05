1389c99212a1a9e4f5876a7caf9c689f8dee4710
osman144 <osman144@umn.edu>
2018-04-04 21:26:52 -0500

    initial changes

---
+++ script.js
@@ -0,0 +1,41 @@
+class Vec {
+    constructor(x=0, y=0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Rect 
+{
+    constructor(w,h)
+    {
+        this.pos = new Vec;
+        this.size = new Vec (w,h);
+    }
+}
+
+class Ball extends Rect 
+{
+    constructor()
+    {
+        super(10,10);
+        this.vel = new Vec;
+    }
+}
+
+const canvas = document.getElementById('pong');
+const context = canvas.getContext('2d');
+const ball = new Ball;
+ball.pos.x = 100;
+ball.pos.y = 50;
+
+function update(dt){
+    ball.pos.x += ball.vel.x * dt;
+    ball.pos.y += ball.vel.y * dt;
+    context.fillStyle = '#000';
+    context.fillRect(0,0, canvas.width, canvas.height);
+
+    context.fillStyle = '#fff';
+    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y)
+}
